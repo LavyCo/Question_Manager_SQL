6bfb312cb12ccaa3e930ed8b05e12490f4cca459
asherb93 <ashercsa@gmail.com>
2022-04-25 18:05:42 +0300

    test

--- Exam.java
+++ Exam.java
@@ -28,35 +28,3 @@ public class Exam {
         return sb.toString();
     }
 }
-=======
-package id206214280_id316650399;
-
-public class Exam {
-    private int numOfQuestions;
-    private  Questions[] questionsExamArray =new Questions[numOfQuestions];
-    public Exam(int numOfQuestions){
-        this.numOfQuestions=numOfQuestions;
-        this.questionsExamArray =new Questions[numOfQuestions];
-    }
-    public int getNumOfQuestions(){
-        return numOfQuestions;
-    }
-    public Questions[] getQuestionsExamArray(){
-        return questionsExamArray;
-    }
-
-    @Override
-    public String toString() {
-        StringBuffer sb =new StringBuffer("the exam is:\n");
-        for(int i=0;i<numOfQuestions;i++){
-            if (questionsExamArray[i] instanceof AmericanQuestions){
-                sb.append(((AmericanQuestions) questionsExamArray[i]).toString()+"\n");
-            }
-            if (questionsExamArray[i] instanceof OpenQuestions){
-                sb.append(((OpenQuestions) questionsExamArray[i]).toString()+"\n");
-            }
-        }
-        return sb.toString();
-    }
-}
->>>>>>> Stashed changes
