40a23643b9cd7c2bec85a92902a27e18e3dfd0e8
LavyCo <101973482+LavyCo@users.noreply.github.com>
2023-01-15 10:40:37 +0200

    Revert "SQL 1"
    This reverts commit 6718592156f875bf6a46f1d397037221bfe8a05a.

--- id206214280_id316650399/Exam.java
+++ id206214280_id316650399/Exam.java
@@ -14,20 +14,11 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Exam<examQuestionArray> implements Cloneable, Serializable {
-
-
-    private static int count=1;
-
-
-    protected int examId;
-    private String examName;
     private int numOfQuestions;
     private ArrayList<Questions> examQuestionArray;
 
     public Exam() {
-
         examQuestionArray = new ArrayList<>();
-
     }
 
     public boolean addQuestion(Questions newQuestion) {
@@ -141,15 +132,6 @@ public class Exam<examQuestionArray> implements Cloneable, Serializable {
     }
 
 
-    public  int getExamId() {
-        return this.examId;
-    }
-
-    public  void setExamId(int examId) {
-        this.examId=count++;
-    }
-
-
     @Override
     public String toString() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
@@ -175,7 +157,4 @@ public class Exam<examQuestionArray> implements Cloneable, Serializable {
         }
         return sb.toString();
     }
-
 }
-
-
